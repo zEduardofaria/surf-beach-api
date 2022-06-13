8bd133f2172aec5d0db289d979416e7f330464bf
Eduardo Faria <z.eduardofaria@gmail.com>
2022-06-12 21:16:02 -0300

    Mocking api client get weather information

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,18 @@
+import { StormGlass } from '@src/clients/stormGlass'
+import axios from 'axios'
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json'
+
+describe('StormFlass client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33.792726
+    const lng = -33.792726
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture)
+
+    const stormGlass = new StormGlass(axios)
+    const response = await stormGlass.fetchPoints(lat, lng)
+
+    expect(response).toEqual(stormGlassNormalized3HoursFixture)
+  })
+})
