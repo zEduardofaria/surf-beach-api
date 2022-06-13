538aa49ed5ba9e2ea36ca493a0fb210567a9d096
Eduardo Faria <z.eduardofaria@gmail.com>
2022-06-12 22:14:47 -0300

    wip: Error handling when consuming external api

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -3,18 +3,54 @@ import axios from 'axios'
 import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
 import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json'
 
+jest.mock('axios')
+
 describe('StormFlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>
+
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726
     const lng = -33.792726
 
-    axios.get = jest
-      .fn()
-      .mockResolvedValue({ data: stormGlassWeather3HoursFixture })
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture })
 
-    const stormGlass = new StormGlass(axios)
+    const stormGlass = new StormGlass(mockedAxios)
     const response = await stormGlass.fetchPoints(lat, lng)
 
     expect(response).toEqual(stormGlassNormalized3HoursFixture)
+  })
+
+  it('should exclude incomplete data points', async () => {
+    const lat = -33.792726
+    const lng = 151.289824
+    const incompleteResponse = {
+      hours: [
+        {
+          windDirection: {
+            noaa: 300,
+          },
+          time: '2020-04-26T00:00:00+00:00',
+        },
+      ],
+    }
+    mockedAxios.get.mockResolvedValue({ data: incompleteResponse })
+
+    const stormGlass = new StormGlass(mockedAxios)
+    const response = await stormGlass.fetchPoints(lat, lng)
+
+    expect(response).toEqual([])
+  })
+
+  it('should get a generic error from StormGlass service when the request fail before reaching the service', async () => {
+    const lat = -33.792726
+    const lng = 151.289824
+
+    mockedAxios.get.mockRejectedValue({ message: 'Network Error' })
+
+    const stormGlass = new StormGlass(mockedAxios)
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error when trying to communicate to StormGlass: Network Error'
+    )
   })
 })
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,4 +1,5 @@
-import { AxiosStatic } from 'axios'
+import { InternalError } from '@src/util/errors/internal-error'
+import { AxiosError, AxiosStatic } from 'axios'
 
 export interface StormGlassPointSource {
   [key: string]: number
@@ -30,6 +31,28 @@ export interface ForecastPoint {
   windSpeed: number
 }
 
+export class StormGlassUnexpectedResponseError extends InternalError {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+export class ClientRequestError extends InternalError {
+  constructor(message: string) {
+    const internalMessage = `Unexpected error when trying to communicate to StormGlass`
+
+    super(`${internalMessage}: ${message}`)
+  }
+}
+
+export class StormGlassResponseError extends InternalError {
+  constructor(message: string) {
+    const internalMessage = `Unexpected error returned by the StormGlass service`
+
+    super(`${internalMessage}: ${message}`)
+  }
+}
+
 export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed'
@@ -38,11 +61,29 @@ export class StormGlass {
   constructor(protected request: AxiosStatic) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
-    const response = await this.request.get<StormGlassForecastResponse>(
-      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}&v=1EsZ5m_bl5o`
-    )
+    try {
+      const response = await this.request.get<StormGlassForecastResponse>(
+        `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}&v=1EsZ5m_bl5o`
+      )
 
-    return this.normalizeResponse(response.data)
+      return this.normalizeResponse(response.data)
+    } catch (err) {
+      const axiosError = err as AxiosError
+
+      if (
+        axiosError instanceof Error &&
+        axiosError.response &&
+        axiosError.response.status
+      ) {
+        throw new StormGlassResponseError(
+          `Error: ${JSON.stringify(axiosError.response.data)} Code: ${
+            axiosError.response.status
+          }`
+        )
+      }
+
+      throw new ClientRequestError((err as { message: any }).message)
+    }
   }
 
   private normalizeResponse(
