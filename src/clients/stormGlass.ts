a96e345bb6c9d378ba7d664e8e6031328f5a395f
Eduardo Faria <z.eduardofaria@gmail.com>
2022-06-18 18:05:39 -0300

    adding request client and typescript generics

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,6 +1,6 @@
-import { InternalError } from '@src/util/errors/internal-error'
-import { AxiosError, AxiosStatic } from 'axios'
 import config, { IConfig } from 'config'
+import { InternalError } from '@src/util/errors/internal-error'
+import * as HTTPUtil from '@src/util/request'
 
 export interface StormGlassPointSource {
   [key: string]: number
@@ -61,7 +61,7 @@ export class StormGlass {
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed'
   readonly stormGlassAPISource = 'noaa'
 
-  constructor(protected request: AxiosStatic) {}
+  constructor(protected request = new HTTPUtil.Request()) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
     try {
@@ -80,21 +80,16 @@ export class StormGlass {
 
       return this.normalizeResponse(response.data)
     } catch (err) {
-      const axiosError = err as AxiosError
-
-      if (
-        axiosError instanceof Error &&
-        axiosError.response &&
-        axiosError.response.status
-      ) {
+      if (err instanceof Error && HTTPUtil.Request.isRequestError(err)) {
+        const error = HTTPUtil.Request.extractErrorData(err)
         throw new StormGlassResponseError(
-          `Error: ${JSON.stringify(axiosError.response.data)} Code: ${
-            axiosError.response.status
-          }`
+          `Error: ${JSON.stringify(error.data)} Code: ${error.status}`
         )
       }
-
-      throw new ClientRequestError((err as { message: any }).message)
+      /**
+       * All the other errors will fallback to a generic client error
+       */
+      throw new ClientRequestError(JSON.stringify(err))
     }
   }
 
